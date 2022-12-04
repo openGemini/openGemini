3e6e21a2502f256fafa00eec1d5a40c240e4d169
XiangYu <49023462+xiangyu5632@users.noreply.github.com>
2022-12-04 23:16:53 +0800

    chore(docker): supports container deployment such as KubeEdge and K8S (#86)
    1. add dockerfile, but sometimes you need change 'From'
    2. add deployment scripts
    
    Signed-off-by: xiangyu5632 <xiangyu5632@126.com>

---
+++ docker/scripts/build_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash/env bash
+
+config=/home/openGemini/config/gemini.conf
+domain=`echo $OPEN_GEMINI_DOMAIN`
+
+configMount=`echo $OPEN_GEMINI_CONFIG`
+if [[ "$configMount" == "" ]]; then
+	echo "Missing environment variable: CONFIG_MOUNT_PATH"
+	exit 1
+fi
+
+if [[ !-f $configMount ]]; then
+	echo "Configuration file does not exist: $configMount"
+	exit 1
+fi
+
+cp -f $configMount $config
+localAddr=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+sed -i "s/{local_addr}/$localAddr/g" $config
+sed -i "s/{domain}/$domain/g" $config
+
