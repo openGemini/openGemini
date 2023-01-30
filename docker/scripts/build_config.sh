edd82d965422e061e2783763b9916fb1675af17a
fx408 <fx4084@gmail.com>
2023-01-30 19:17:48 +0800

    修复语法错误
    1. 修复 if 语句语法错误
    2. 修复 多网卡（IP）情况下 sed 语法错误
    
    Signed-off-by: fx408 <fx4084@gmail.com>

--- docker/scripts/build_config.sh
+++ docker/scripts/build_config.sh
@@ -5,17 +5,17 @@ domain=`echo $OPEN_GEMINI_DOMAIN`
 
 configMount=`echo $OPEN_GEMINI_CONFIG`
 if [[ "$configMount" == "" ]]; then
-	echo "Missing environment variable: CONFIG_MOUNT_PATH"
+	echo "Missing environment variable: OPEN_GEMINI_CONFIG"
 	exit 1
 fi
 
-if [[ !-f $configMount ]]; then
+if [[ ! -f $configMount ]]; then
 	echo "Configuration file does not exist: $configMount"
 	exit 1
 fi
 
 cp -f $configMount $config
-localAddr=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+localAddr=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2;exit}'|tr -d "addr:"`
 sed -i "s/{{addr}}/$localAddr/g" $config
 sed -i "s/{{domain}}/$domain/g" $config
 
