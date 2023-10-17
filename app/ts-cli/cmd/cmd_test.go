4b0a368fa70b66dbbc61fe01b1b833af8e76bf93
xmh <1197843839@qq.com>
2023-10-17 06:15:42 -0500

    refactor: ts-cli commands and flags with cobra (#412)
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- app/ts-cli/cmd/cmd_test.go
+++ app/ts-cli/cmd/cmd_test.go
@@ -17,10 +17,11 @@ limitations under the License.
 package cmd
 
 import (
-	"os"
+	"strconv"
 	"testing"
 
 	"github.com/influxdata/influxdb/pkg/testing/assert"
+	"github.com/openGemini/openGemini/app/ts-cli/geminicli"
 	"github.com/spf13/cobra"
 )
 
@@ -38,110 +39,60 @@ var (
 func TestCobraCmd(t *testing.T) {
 	for _, tc := range []struct {
 		name      string
-		args      []string
+		options   *geminicli.CommandLineConfig
 		validator func()
 	}{
 		{
 			name: "test cobra flags",
-			args: []string{
-				"--host",
-				"192.168.0.1",
-				"--port",
-				"1234",
-				"--socket",
-				"unix",
-				"--socket",
-				"unix",
-				"--username",
-				"root",
-				"--password",
-				"password",
-				"--database",
-				"db0",
-				"--ssl",
-				"true",
-				"--unsafeSsl",
-				"true",
-				"--import",
-				"--path",
-				"path",
+			options: &geminicli.CommandLineConfig{
+				Host:       "192.168.0.1",
+				Port:       1234,
+				UnixSocket: "unix",
+				Username:   "root",
+				Password:   "password",
+				Database:   "db0",
+				Ssl:        true,
+				IgnoreSsl:  true,
 			},
 			validator: func() {
-				assert.Equal(t, gFlags.Host, "192.168.0.1")
-				assert.Equal(t, gFlags.Port, 1234)
-				assert.Equal(t, gFlags.UnixSocket, "unix")
-				assert.Equal(t, gFlags.Username, "root")
-				assert.Equal(t, gFlags.Password, "password")
-				assert.Equal(t, gFlags.Database, "db0")
-				assert.Equal(t, gFlags.Ssl, true)
-				assert.Equal(t, gFlags.IgnoreSsl, true)
-				assert.Equal(t, gFlags.IgnoreSsl, true)
-				assert.Equal(t, gFlags.Import, true)
-				assert.Equal(t, gFlags.Path, "path")
+				assert.Equal(t, options.Host, testCmd.Flags().Lookup("host").Value.String())
+				assert.Equal(t, strconv.Itoa(options.Port), testCmd.Flags().Lookup("port").Value.String())
+				assert.Equal(t, options.UnixSocket, testCmd.Flags().Lookup("socket").Value.String())
+				assert.Equal(t, options.Username, testCmd.Flags().Lookup("username").Value.String())
+				assert.Equal(t, options.Password, testCmd.Flags().Lookup("password").Value.String())
+				assert.Equal(t, options.Database, testCmd.Flags().Lookup("database").Value.String())
+				assert.Equal(t, strconv.FormatBool(options.Ssl), testCmd.Flags().Lookup("ssl").Value.String())
+				assert.Equal(t, strconv.FormatBool(options.IgnoreSsl), testCmd.Flags().Lookup("unsafeSsl").Value.String())
 			},
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			os.Args = os.Args[0:1]
-			os.Args = append(os.Args, tc.args...)
-			bindFlags(testCmd, &gFlags)
-			testCmd.Execute()
+			initFlags(testCmd, tc.options)
+			err := testCmd.Execute()
+			if err != nil {
+				return
+			}
 			tc.validator()
 		})
 	}
 }
 
-func TestCapatibleCmd(t *testing.T) {
-	for _, tc := range []struct {
-		name      string
-		args      []string
-		validator func()
-	}{
-		{
-			name: "test capatible flags",
-			args: []string{
-				"-host",
-				"192.168.0.1",
-				"-port",
-				"1234",
-				"-socket",
-				"unix",
-				"-username",
-				"root",
-				"-password",
-				"password",
-				"-database",
-				"db0",
-				"-ssl",
-				"-unsafeSsl",
-				"-import",
-				"-path",
-				"path",
-			},
-			validator: func() {
-				assert.Equal(t, gFlags.Host, "192.168.0.1")
-				assert.Equal(t, gFlags.Port, 1234)
-				assert.Equal(t, gFlags.UnixSocket, "unix")
-				assert.Equal(t, gFlags.Username, "root")
-				assert.Equal(t, gFlags.Password, "password")
-				assert.Equal(t, gFlags.Database, "db0")
-				assert.Equal(t, gFlags.Ssl, true)
-				assert.Equal(t, gFlags.IgnoreSsl, true)
-				assert.Equal(t, gFlags.Import, true)
-				assert.Equal(t, gFlags.Path, "path")
-			},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			os.Args = os.Args[0:1]
-			os.Args = append(os.Args, tc.args...)
-			compatibleCmd.Bind(&gFlags)
-			if err := compatibleCmd.Parse(os.Args[1:]); err != nil {
-				t.Error(err)
-			}
-			unknownArgs := compatibleCmd.Args()
-			assert.Equal(t, len(unknownArgs), 0)
-			tc.validator()
-		})
+func initFlags(cmd *cobra.Command, options *geminicli.CommandLineConfig) {
+	cmd.Flags().StringVar(&options.Host, "host", DEFAULT_HOST, "openGemini host to connect to.")
+	cmd.Flags().IntVar(&options.Port, "port", DEFAULT_PORT, "openGemini port to connect to.")
+	cmd.Flags().StringVar(&options.UnixSocket, "socket", "", "openGemini unix domain socket to connect to.")
+	cmd.Flags().StringVarP(&options.Username, "username", "u", "", "Username to connect to openGemini.")
+	cmd.Flags().StringVarP(&options.Password, "password", "p", "", "Password to connect to openGemini.")
+	cmd.Flags().StringVar(&options.Database, "database", "", "Database to connect to openGemini.")
+	cmd.Flags().BoolVar(&options.Ssl, "ssl", false, "Use https for connecting to openGemini.")
+	cmd.Flags().BoolVar(&options.IgnoreSsl, "unsafeSsl", true, "Ignore ssl verification when connecting openGemini by https.")
+	cmd.Flags().StringVar(&options.Precision, "precision", "ns", "Precision specifies the format of the timestamp: rfc3339,h,m,s,ms,u or ns.")
+}
+
+func TestExecute(t *testing.T) {
+	rootCmd = testCmd
+	Execute()
+	if err := rootCmd.Execute(); err != nil {
+		t.Errorf("execute error: %v", err)
 	}
 }
