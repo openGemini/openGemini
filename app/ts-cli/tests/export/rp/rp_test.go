6bbe66786ecff3a14ee7ed16f4a2706e64b81cf6
charminghacker <charminghacker@163.com>
2024-09-22 18:03:44 +0800

    fix: licence check
    Signed-off-by: charminghacker <charminghacker@163.com>

--- app/ts-cli/tests/export/rp/rp_test.go
+++ app/ts-cli/tests/export/rp/rp_test.go
@@ -1,4 +1,4 @@
-// Copyright right 2024 openGemini author.
+// Copyright 2024 Huawei Cloud Computing Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
