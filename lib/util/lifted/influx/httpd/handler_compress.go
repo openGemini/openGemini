4992ee4c0c79e211c95184533026497f4344e3d4
xkx <triumph_9431@qq.com>
2024-12-06 14:38:01 +0000

    feat(compress): support snappy compression
    Signed-off-by: xkx <triumph_9431@qq.com>

--- lib/util/lifted/influx/httpd/handler_compress.go
+++ lib/util/lifted/influx/httpd/handler_compress.go
@@ -19,11 +19,11 @@ import (
 	"io"
 	"net/http"
 	"strings"
-	"sync"
 
 	"github.com/golang/snappy"
 	"github.com/klauspost/compress/zstd"
-	"github.com/openGemini/openGemini/lib/cpu"
+	compression "github.com/openGemini/openGemini/lib/compress"
+	"github.com/pierrec/lz4/v4"
 )
 
 type lazyCompressResponseWriter struct {
@@ -38,23 +38,29 @@ type lazyCompressResponseWriter struct {
 func compressFilter(inner http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var writer io.Writer = w
+		// todo: consider Content_negotiation: https://developer.mozilla.org/en-US/docs/Web/HTTP/Content_negotiation
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		switch {
 		case strings.Contains(acceptEncoding, "gzip"):
-			gz := getGzipWriter(w)
+			gz := compression.GetGzipWriter(w)
 			defer gz.Close()
 			writer = gz
 			w.Header().Set("Content-Encoding", "gzip")
 		case strings.Contains(acceptEncoding, "zstd"):
-			enc := getZstdWriter(w)
+			enc := compression.GetZstdWriter(w)
 			defer enc.Close()
 			writer = enc
 			w.Header().Set("Content-Encoding", "zstd")
 		case strings.Contains(acceptEncoding, "snappy"):
-			sn := getSnappyWriter(w)
+			sn := compression.GetSnappyWriter(w)
 			defer sn.Close()
 			writer = sn
 			w.Header().Set("Content-Encoding", "snappy")
+		case strings.Contains(acceptEncoding, "lz4"):
+			lz := compression.GetLz4Writer(w)
+			defer lz.Close()
+			writer = lz
+			w.Header().Set("Content-Encoding", "lz4")
 		default:
 			inner.ServeHTTP(w, r)
 			return
@@ -107,181 +113,16 @@ func (w *lazyCompressResponseWriter) Flush() {
 
 func (w *lazyCompressResponseWriter) Close() error {
 	if gw, ok := w.Writer.(*gzip.Writer); ok {
-		putGzipWriter(gw)
+		compression.PutGzipWriter(gw)
 	}
 	if zw, ok := w.Writer.(*zstd.Encoder); ok {
-		putZstdWriter(zw)
+		compression.PutZstdWriter(zw)
+	}
+	if sw, ok := w.Writer.(*snappy.Writer); ok {
+		compression.PutSnappyWriter(sw)
+	}
+	if lw, ok := w.Writer.(*lz4.Writer); ok {
+		compression.PutLz4Writer(lw)
 	}
 	return nil
 }
-
-type FixedCachePool struct {
-	cache chan interface{}
-	sp    sync.Pool
-}
-
-func NewFixedCachePool(size int, newFunc func() interface{}) *FixedCachePool {
-	return &FixedCachePool{
-		cache: make(chan interface{}, size),
-		sp: sync.Pool{
-			New: newFunc,
-		},
-	}
-}
-
-func (p *FixedCachePool) Get() interface{} {
-	select {
-	case item := <-p.cache:
-		return item
-	default:
-		item := p.sp.Get()
-		if item == nil {
-			item = p.sp.New()
-		}
-		return item
-	}
-}
-
-func (p *FixedCachePool) Put(item interface{}) {
-	select {
-	case p.cache <- item:
-	default:
-		p.sp.Put(item)
-	}
-}
-
-// ******************** region gzip write pool ***********************
-type GzipWriterPool struct {
-	pool *FixedCachePool
-}
-
-func NewGzipWriterPool() *GzipWriterPool {
-	p := &GzipWriterPool{
-		pool: NewFixedCachePool(cpu.GetCpuNum()*2, func() interface{} {
-			return gzip.NewWriter(nil)
-		}),
-	}
-	return p
-}
-
-func (p *GzipWriterPool) Get() *gzip.Writer {
-	return p.pool.Get().(*gzip.Writer)
-}
-
-func (p *GzipWriterPool) Put(gz *gzip.Writer) {
-	p.pool.Put(gz)
-}
-
-var gzipWriterPool = NewGzipWriterPool()
-
-func getGzipWriter(w io.Writer) *gzip.Writer {
-	gz := gzipWriterPool.Get()
-	gz.Reset(w)
-	return gz
-}
-
-func putGzipWriter(gz *gzip.Writer) {
-	gz.Close()
-	gzipWriterPool.Put(gz)
-}
-
-// ******************** endregion gzip write pool ***********************
-
-// ******************** region gzip read pool ***************************
-
-func GetGzipReader(r io.Reader) (*gzip.Reader, error) {
-	v := gzipReaderPool.Get()
-	if v == nil {
-		return gzip.NewReader(r)
-	}
-	zr := v.(*gzip.Reader)
-	if err := zr.Reset(r); err != nil {
-		return nil, err
-	}
-	return zr, nil
-}
-
-// PutGzipReader returns back gzip reader obtained via GetGzipReader.
-func PutGzipReader(zr *gzip.Reader) {
-	_ = zr.Close()
-	gzipReaderPool.Put(zr)
-}
-
-var gzipReaderPool sync.Pool
-
-// ******************** endregion gzip read pool ***************************
-
-// ******************** region zstd write pool ***********************
-type ZstdWriterPool struct {
-	pool *FixedCachePool
-}
-
-func NewZstdWriterPool() *ZstdWriterPool {
-	p := &ZstdWriterPool{
-		pool: NewFixedCachePool(cpu.GetCpuNum()*2, func() interface{} {
-			encoder, _ := zstd.NewWriter(nil)
-			return encoder
-		}),
-	}
-	return p
-}
-
-func (p *ZstdWriterPool) Get() *zstd.Encoder {
-	return p.pool.Get().(*zstd.Encoder)
-}
-
-func (p *ZstdWriterPool) Put(zstdEncoder *zstd.Encoder) {
-	p.pool.Put(zstdEncoder)
-}
-
-var zstdWriterPool = NewZstdWriterPool()
-
-func getZstdWriter(w io.Writer) *zstd.Encoder {
-	zstdEncoder := zstdWriterPool.Get()
-	zstdEncoder.Reset(w)
-	return zstdEncoder
-}
-
-func putZstdWriter(zstdEncoder *zstd.Encoder) {
-	zstdEncoder.Close()
-	zstdWriterPool.Put(zstdEncoder)
-}
-
-// ******************** endregion zstd write pool ***********************
-
-// ******************** region snappy write pool ***************************
-type SnappyWriterPool struct {
-	pool *FixedCachePool
-}
-
-func NewSnappyWriterPool() *SnappyWriterPool {
-	p := &SnappyWriterPool{
-		pool: NewFixedCachePool(cpu.GetCpuNum()*2, func() interface{} {
-			return snappy.NewBufferedWriter(nil)
-		}),
-	}
-	return p
-}
-
-func (p *SnappyWriterPool) Get() *snappy.Writer {
-	return p.pool.Get().(*snappy.Writer)
-}
-
-func (p *SnappyWriterPool) Put(snappyWriter *snappy.Writer) {
-	p.pool.Put(snappyWriter)
-}
-
-var snappyWriterPool = NewSnappyWriterPool()
-
-func getSnappyWriter(w io.Writer) *snappy.Writer {
-	snappyWriter := snappyWriterPool.Get()
-	snappyWriter.Reset(w)
-	return snappyWriter
-}
-
-func putSnappyWriter(snappyWriter *snappy.Writer) {
-	snappyWriter.Close()
-	snappyWriterPool.Put(snappyWriter)
-}
-
-// ******************** endregion snappy write pool ***************************
