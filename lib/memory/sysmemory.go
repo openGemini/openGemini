b1232c7a90198814f2a7374fa297f73ac96cbf4d
royal_111 <royal_111@qq.com>
2025-07-21 17:01:07 +0800

    fix: memory size on older Linux kernel
    Signed-off-by: royal_111 <royal_111@qq.com>

--- lib/memory/sysmemory.go
+++ lib/memory/sysmemory.go
@@ -15,144 +15,80 @@
 package memory
 
 import (
-	"bytes"
-	"os"
-	"runtime"
 	"sync"
 	"time"
 
-	"github.com/openGemini/openGemini/lib/util"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
-const maxMemUse = 64 * 1024 * 1024 * 1024
+type memoryMonitor struct {
+	lastGetTime    time.Time // the last time the memory was read
+	totalCache     int64     // total memory cache
+	availableCache int64     // available memory cache
+	lock           sync.RWMutex
+}
 
-var lastGetTime time.Time
-var sysMemTotal, sysMemFree, totalMemoryMax int64
-var readMemMu sync.Mutex
+var (
+	memMonitor *memoryMonitor // singleton
+)
 
 func init() {
-	sysMemTotal, sysMemFree = ReadSysMemory()
-	totalMemoryMax = sysMemTotal
-	lastGetTime = time.Now()
+	memMonitor = newMemMonitor()
 }
 
-func ReadSysMemory() (int64, int64) {
-	if runtime.GOOS == "linux" {
-		return ReadSysMemoryLinux()
+func newMemMonitor() *memoryMonitor {
+	total, available := ReadSysMemory()
+	m := &memoryMonitor{
+		lastGetTime:    time.Now(),
+		totalCache:     total,
+		availableCache: available,
 	}
-	info, _ := mem.VirtualMemory()
-	return int64(info.Total / 1024), int64(info.Available / 1024)
+	return m
 }
 
-func ReadSysMemoryLinux() (total int64, available int64) {
-	var buf [256]byte
-	n1 := readSysMemInfo(buf[:])
-	if n1 != 0 {
-
-		// the first 256 bytes
-		// MemTotal
-		mTotalLeft := bytes.Index(buf[:], []byte("MemTotal"))
-		// MemAvailable
-		mAvailableLeft := bytes.Index(buf[:], []byte("MemAvailable"))
-		// MemFree
-		mFreeLeft := bytes.Index(buf[:], []byte("MemFree"))
-		// Buffers
-		mBuffersLeft := bytes.Index(buf[:], []byte("Buffers"))
-		// Cached
-		mCachedLeft := bytes.Index(buf[:], []byte("Cached"))
-
-		if mTotalLeft == -1 {
-			return maxMemUse, maxMemUse
-		}
-		total = getMemInt64(buf[:], "MemTotal:", mTotalLeft)
-
-		if mAvailableLeft != -1 {
-			available = getMemInt64(buf[:], "MemAvailable:", mAvailableLeft)
-			return
-		}
-
-		// lower kernel system
-		// MemAvailable = MemFree + Buffers + Cached
-		if mFreeLeft != -1 {
-			available += getMemInt64(buf[:], "MemFree:", mFreeLeft)
-		}
-
-		if mBuffersLeft != -1 {
-			available += getMemInt64(buf[:], "Buffers:", mBuffersLeft)
-		}
-
-		if mCachedLeft != -1 {
-			available += getMemInt64(buf[:], "Cached:", mCachedLeft)
-		}
-
-		return
-	}
-	/*
-		output like:
-		MemTotal:       32505856 kB
-		MemFree:        28917428 kB
-		MemAvailable:   29288348 kB
-		Buffers:               0 kB
-		Cached:           370920 kB
-		SwapCached:            0 kB
-		Active:          3422876 kB
-	*/
-	return maxMemUse, maxMemUse
+// get memory monitor singleton
+func GetMemMonitor() MemoryMonitor {
+	return memMonitor
 }
-func SysMem() (total, free int64) {
-	t := time.Now()
-	readMemMu.Lock()
-	defer readMemMu.Unlock()
-	if t.Sub(lastGetTime) < 100*time.Millisecond {
-		total, free = sysMemTotal, sysMemFree
+
+// get the total amount of memory and remaining capacity (total, available (kB))
+func (m *memoryMonitor) SysMem() (total, available int64) {
+	now := time.Now()
+	m.lock.RLock()
+	if now.Sub(m.lastGetTime) < cacheInterval {
+		total, available = m.totalCache, m.availableCache
+		m.lock.RUnlock()
 		return
 	}
-	total, free = ReadSysMemory()
-	if total <= 0 || free <= 0 {
-		total, free = totalMemoryMax, totalMemoryMax
-		return
+	m.lock.RUnlock()
+
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	now = time.Now() // double check
+	if now.Sub(m.lastGetTime) < cacheInterval {
+		return m.totalCache, m.availableCache
 	}
-	lastGetTime = t
-	sysMemTotal = total
-	sysMemFree = free
+
+	total, available = ReadSysMemory()
+	m.lastGetTime = now
+	m.totalCache = total
+	m.availableCache = available
+
 	return
 }
 
-func readSysMemInfo(buf []byte) int {
-	f, err := os.Open("/proc/meminfo")
-	if err != nil {
-		return 0
-	}
-	defer util.MustClose(f)
-
-	n, err := f.ReadAt(buf, 0)
-	if err != nil || n < len(buf) {
-		return 0
-	}
-	return n
-}
-
-func bytes2Int(b []byte) int64 {
-	var v int64
-	for _, c := range b {
-		v = v*10 + int64(c-'0')
-	}
-	return v
-}
-
-func MemUsedPct() float64 {
-	total, available := SysMem()
+// memory usage percentage
+func (m *memoryMonitor) MemUsedPct() float64 {
+	total, available := m.SysMem()
 	return (1 - float64(available)/float64(total)) * 100
 }
 
-// converts a memory value string (e.g., "MemTotal:     789745 kB") into an integer (e.g., 789745).
-func getMemInt64(buf []byte, prefix string, left int) int64 {
-	left += len(prefix)
-	right := bytes.Index(buf[left:], []byte("kB")) + left
-	if left > right {
-		return 0
+// return total available memory (kB)
+func ReadSysMemory() (int64, int64) {
+	if info, err := mem.VirtualMemory(); err != nil { // can not read memory, block it.
+		return defaultMaxMem, 0
+	} else {
+		return int64(info.Total >> 10), int64(info.Available >> 10)
 	}
-	mByte := bytes.TrimSpace(buf[left:right]) // [left, right)
-	return bytes2Int(mByte)
 }
--- lib/memory/sysmemory_test.go
+++ lib/memory/sysmemory_test.go
@@ -12,108 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package memory
+package memory_test
 
 import (
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/openGemini/openGemini/lib/memory"
 	"github.com/stretchr/testify/require"
 )
 
 func TestReadSysMemory(t *testing.T) {
-	total, available := ReadSysMemory()
-	MemUsedPct()
-	readSysMemInfo(nil)
+	total, available := memory.ReadSysMemory()
+	memUsed := memory.GetMemMonitor().MemUsedPct()
+	t.Log(total, available, memUsed)
 	require.NotEmpty(t, total)
 	require.NotEmpty(t, available)
+	require.NotEmpty(t, memUsed)
 }
 
-func BenchmarkReadSysMemory(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		MemUsedPct()
-	}
-}
-
-func BenchmarkReadSysMemory_Parallel(b *testing.B) {
-	b.ReportAllocs()
+func TestSysMem(t *testing.T) {
 	var wg sync.WaitGroup
-	f := func() {
-		defer wg.Done()
-		for i := 0; i < b.N; i++ {
-			MemUsedPct()
-		}
-	}
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 100; i++ {
+		time.Sleep(time.Millisecond)
 		wg.Add(1)
-		go f()
+		go func() {
+			defer wg.Done()
+			memory.GetMemMonitor().SysMem() // t.Log(memory.GetMemMonitor().SysMem())
+		}()
 	}
 	wg.Wait()
-}
-
-func TestGetMemInt64(t *testing.T) {
-
-	type testCase struct {
-		info   string
-		prefix string
-		left   int
-		ans    int64
-	}
-
-	testCases := []testCase{
-		{
-			"MemTotal:       14528572 kB",
-			"MemTotal:",
-			0,
-			14528572,
-		},
-		{
-			"MemFree:         6194012 kB",
-			"MemFree:",
-			0,
-			6194012,
-		},
-		{
-			"MemAvailable:    6254489 kB",
-			"MemAvailable:",
-			0,
-			6254489,
-		},
-		{
-			"Buffers:           34032 kB",
-			"Buffers:",
-			0,
-			34032,
-		},
-		{
-			"Cached:           188576 kB",
-			"Cached:",
-			0,
-			188576,
-		},
-		{
-			"SwapCached:            0 kB",
-			"SwapCached:",
-			0,
-			0,
-		},
-		{
-			"HugePages_Total:       0",
-			"HugePages_Total:",
-			0,
-			0,
-		},
-		{
-			"   test:       55 kB",
-			"test:",
-			3,
-			55,
-		},
-	}
-
-	for _, tcase := range testCases {
-		act := getMemInt64([]byte(tcase.info), tcase.prefix, tcase.left)
-		require.Equal(t, tcase.ans, act)
-	}
 }
